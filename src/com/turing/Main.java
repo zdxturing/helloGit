27fe690218cc526c3c439c692e9fc526abf79f9a
fred <1094424214@qq.com>
2019-10-24 13:36:52 +0800

    项目修改了

--- src/com/turing/Main.java
+++ src/com/turing/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello Git");
+
+        for (int i = 0; i < 10; i++)
+            System.out.println("hello"+i);
     }
 }
